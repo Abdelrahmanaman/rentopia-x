aecddd9f5d5746a55b9922e8e1466f0e187a2340
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2024-05-14 19:48:11 +0200

    added neon as db

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -25,25 +25,25 @@ async function main() {
   //     });0
   // console.log(data)
   // Apartments
-  await prisma.apartment.create({
-    data: {
-      address: "123 Main St",
-      city: "New York",
-      slug: "apt",
-      rooms: 2,
-      wifi: true,
-      tv: true,
-      price: 000,
-      description: "Cozy apartment in the heart of the city",
-      images: [
-        "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1630699144641-72fa7a6b8aa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1633104069776-ea7e61258ec9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://images.unsplash.com/photo-1630699144867-37acec97df5a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      ],
-      ownerId: "clw6nuxjk0000wcynges0b3ee",
-    },
-  });
+  // await prisma.apartment.create({
+  //   data: {
+  //     address: "123 Main St",
+  //     city: "New York",
+  //     slug: "apt",
+  //     rooms: 2,
+  //     wifi: true,
+  //     tv: true,
+  //     price: 4000,
+  //     description: "Cozy apartment in the heart of the city",
+  //     images: [
+  //       "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //       "https://images.unsplash.com/photo-1630699144641-72fa7a6b8aa1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //       "https://images.unsplash.com/photo-1633104069776-ea7e61258ec9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //       "https://images.unsplash.com/photo-1630699144867-37acec97df5a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     ],
+  //     ownerId: "clw6nuxjk0000wcynges0b3ee",
+  //   },
+  // });
   // Favourites
   // await prisma.favourite.create({
   //     data: {
