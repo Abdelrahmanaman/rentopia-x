a9d8d81c965bbfe359e39d1382a7156bf2e1edd8
abdelrahmanaman <abdelrahman.bourka1@gmail.com>
2024-05-14 14:20:17 +0200

    commented the form for deployment

--- src/app/add-apartment/page.tsx
+++ src/app/add-apartment/page.tsx
@@ -1,9 +1,9 @@
-import ApartmentForm from "@/components/ApartmentForm";
+// import ApartmentForm from "@/components/ApartmentForm";
 
 export default function page() {
   return (
     <section className="max-w-6xl mx-auto">
-      <ApartmentForm />
+      {/* <ApartmentForm /> */}
     </section>
   );
 }
